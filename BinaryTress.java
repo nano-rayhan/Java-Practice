ae0c3f2aa39f3c0e1f2e07594b43a4e0a3065cd6
nano-rayhan <rr5345933@gmail.com>
2025-04-17 00:00:12 +0600

    Binaray tree

---
+++ BinaryTress.java
@@ -0,0 +1,60 @@
+public class BinaryTress {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node (int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    static class Binarytree{
+        static int idx = -1;
+        public static Node buildTree(int nodes[]){
+            idx++;
+            if(nodes[idx] == -1){
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    } 
+    public static void preorder(Node root){
+        if(root == null){
+            return;
+        }
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static void inorder(Node root){
+        if(root == null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void postorder(Node root){
+        if(root == null){
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data + " ");
+    }
+    public static void main(String[] args) {
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        Binarytree tree = new Binarytree();
+        Node root = tree.buildTree(nodes);
+        postorder(root);
+    }
+}
--- margeSort.java
+++ margeSort.java
@@ -1,32 +1,32 @@
 public class margeSort {
+    static int count = 0;
     public static void conquer(int arr[],int si, int mid, int ei){
         int marged[] = new int[ei - si +1];
-
         int idx1 = si;
         int idx2 = mid+1;
-        int x = 0;
-
+        int x = 0;       
         while (idx1 <= mid && idx2 <=ei) {
             if(arr[idx1] <= arr[idx2]){
-                marged[x++] = arr[idx1++];
+                marged[x++] = arr[idx1++];               
             }
             else{
                 marged[x++] = arr[idx2++];
+                count += (mid - idx1 + 1);
             }
         }
         while (idx1 <= mid) {
             marged[x++] = arr[idx1++];
+            
         }
         while (idx2 <= ei) {
             marged[x++] = arr[idx2++];
+           
         }
-
         for(int i=0, j=si; i<marged.length; i++, j++){
             arr[j] = marged[i];
-        }
-
-    }
-    
+            
+        }       
+    }   
     public static void divide(int arr[], int si, int ei){
         if(si >= ei){
             return;
@@ -34,16 +34,20 @@ public class margeSort {
         int mid = si + (ei-si)/2;
         divide(arr, si, mid);
         divide(arr, mid+1, ei);
-        conquer(arr, si, mid, ei);
+        conquer(arr, si, mid, ei);   
     }
     public static void main(String[] args) {
         int arr[] = {6,2,9,4,8,4};
         int n = arr.length;
-    
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("\n Marged Sort: ");
         divide(arr, 0, n-1);
         for(int i=0;i<n;i++){
             System.out.print(arr[i] + " ");
         }
+        System.out.println("\n"+ count);
         System.out.println();
     }
 }
